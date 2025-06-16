16a463d8251b06ec6f53cd23f03b2e15497f91e0
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-17 00:48:10 +0300

    Refactor ProductColorResource to simplify color attributes in response
    - Replaced individual color attributes ('color_name', 'color_code', 'stock', 'is_active') with a single 'color' key in the response array for improved clarity and consistency.

--- app/Http/Resources/ProductColorResource.php
+++ app/Http/Resources/ProductColorResource.php
@@ -12,10 +12,7 @@ class ProductColorResource extends JsonResource
         return [
             'id' => $this->id,
             'product_id' => $this->product_id,
-            'color_name' => $this->color_name,
-            'color_code' => $this->color_code,
-            'stock' => $this->stock,
-            'is_active' => $this->is_active,
+            'color' => $this->color,
             'product' => new ProductResource($this->whenLoaded('product')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
