3f9efe5bf996960461da3b483f829a47c1fa95f8
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-16 16:36:35 +0300

    Add product management routes to API admin

--- routes/api_admin.php
+++ routes/api_admin.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->middleware(['auth:sanctum', AdminMiddleware::class])->group(function () {
-     
+
+    Route::prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/{id}', [ProductController::class, 'show']);
+        Route::post('/', [ProductController::class, 'create']);
+        Route::put('/{id}', [ProductController::class, 'update']);
+        Route::delete('/{id}', [ProductController::class, 'delete']);
+    });
 });
