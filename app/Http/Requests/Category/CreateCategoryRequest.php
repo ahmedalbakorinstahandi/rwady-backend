b707bd10a6a3fad5084a901e3640fca41bf30d40
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-17 01:22:36 +0300

    Add availability and SEO fields to CreateCategoryRequest for enhanced category creation
    - Introduced 'availability' as a nullable boolean field.
    - Added 'seo' as a nullable array with specific attributes: 'meta_title', 'meta_description', 'keywords', and 'image', each with defined validation rules.

--- app/Http/Requests/Category/CreateCategoryRequest.php
+++ app/Http/Requests/Category/CreateCategoryRequest.php
@@ -14,6 +14,12 @@ class CreateCategoryRequest extends BaseFormRequest
             'description' => LanguageService::translatableFieldRules('nullable|string'),
             'parent_id' => 'nullable|exists:categories,id,deleted_at,NULL',
             'image' => 'nullable|string|max:255',
+            'availability' => 'nullable|boolean',
+            'seo' => 'nullable|array',
+            'seo.meta_title' => 'nullable|string|max:255',
+            'seo.meta_description' => 'nullable|string',
+            'seo.keywords' => 'nullable|string|max:255',
+            'seo.image' => 'nullable|string|max:255',
         ];
     }
 } 
