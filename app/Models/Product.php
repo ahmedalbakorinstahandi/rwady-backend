41cbcdca2a131280d7677f4cd63044c5351ab3dc
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-16 19:31:50 +0300

    Update description attribute in Product model to handle nullable values for improved translation support

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -83,7 +83,7 @@ class Product extends Model
     protected function description(): Attribute
     {
         return Attribute::make(
-            get: fn(string $value) => $this->getAllTranslations('description'),
+            get: fn(?string $value) => $this->getAllTranslations('description'),
         );
     }
 
