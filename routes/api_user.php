bd498749c97e84f7ff461af6e02eecb86ad9386d
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-17 12:11:21 +0300

    Add new API routes for products, categories, brands, banners, and featured sections
    - Introduced new route groups for products, categories, brands, banners, and featured sections under the user prefix, enhancing the API structure and organization.
    - Each route group includes index and show methods for better resource management and retrieval.

--- routes/api_user.php
+++ routes/api_user.php
@@ -2,6 +2,10 @@
 
 // prfix user 
 
+use App\Http\Controllers\BannerController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FeaturedSectionController;
 use App\Http\Controllers\HomeSectionController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
@@ -10,7 +14,32 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('user')->group(function () {
 
     Route::get('/home-sections', [HomeSectionController::class, 'index']);
-    Route::get('/products', [ProductController::class, 'index']);
+
+
+    Route::prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/{product}', [ProductController::class, 'show']);
+    });
+
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [CategoryController::class, 'index']);
+        Route::get('/{category}', [CategoryController::class, 'show']);
+    });
+
+    Route::prefix('brands')->group(function () {
+        Route::get('/', [BrandController::class, 'index']);
+        Route::get('/{brand}', [BrandController::class, 'show']);
+    });
+
+    Route::prefix('banners')->group(function () {
+        Route::get('/', [BannerController::class, 'index']);
+        Route::get('/{banner}', [BannerController::class, 'show']);
+    });
+
+    Route::prefix('featured-sections')->group(function () {
+        Route::get('/', [FeaturedSectionController::class, 'index']);
+        Route::get('/{featuredSection}', [FeaturedSectionController::class, 'show']);
+    });
 
     Route::middleware('auth:sanctum')->group(function () {
 
