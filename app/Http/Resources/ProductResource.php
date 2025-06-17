8daddc438f54911efd4db0c9c0a7de5a85675faa
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-17 15:56:34 +0300

    Update related products handling in ProductResource to ensure default values
    - Modified the 'related_products' and 'related_category_products' fields in ProductResource to return an empty collection if not loaded, improving data consistency and preventing potential null errors in API responses.

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -11,8 +11,8 @@ class ProductResource extends JsonResource
     {
 
 
-        $relatedProducts = $this->whenLoaded('relatedProducts');
-        $relatedCategoryProducts = $this->whenLoaded('relatedCategoryProducts');
+        $relatedProducts = $this->whenLoaded('relatedProducts') ?? collect();
+        $relatedCategoryProducts = $this->whenLoaded('relatedCategoryProducts') ?? collect();
 
         $relatedProducts = $relatedProducts->merge($relatedCategoryProducts);
 
