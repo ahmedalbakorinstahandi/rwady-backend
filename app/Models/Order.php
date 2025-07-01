24363e486aae8449c7590f1583edb62eadea008d
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-07-01 14:09:24 +0300

    Update Order model to use MorphOne for address relationship
    - Changed the address relationship in the Order model from HasOne to MorphOne, allowing for more flexible address associations. This update enhances the model's capability to handle polymorphic relationships effectively.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Order extends Model
 {
@@ -114,9 +115,10 @@ class Order extends Model
         return $this->hasMany(OrderPayment::class);
     }
 
-    public function address(): HasOne
+    // morpth has one
+    public function address(): MorphOne
     {
-        return $this->hasOne(Address::class);
+        return $this->morphOne(Address::class, 'addressable');
     }
 
     public function metadata(): Attribute
