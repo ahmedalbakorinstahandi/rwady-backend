ca3b56da0160934d384118bd11b1994bd5f52636
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-08-10 11:44:52 +0300

    Update products_count in CategoryResource to use cached value for improved performance

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryResource extends JsonResource
 {
@@ -18,7 +19,9 @@ class CategoryResource extends JsonResource
             'image_url' => $this->image ? url('storage/' . $this->image) : null,
             'availability' => $this->availability,
             'orders' => $this->orders,
-            'products_count' => 1,
+            'products_count' => Cache::remember("category_{$this->id}_products_count", 3600*24, function () {
+                return $this->products->count();
+            }),
             'parent' => new CategoryResource($this->whenLoaded('parent')),
             'children' => CategoryResource::collection($this->whenLoaded('children')),
             'promotion' => new PromotionResource($this->getBestPromotionAttribute()),
