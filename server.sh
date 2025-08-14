4bcebc9fd7b918c10516c6fd8f7004d590b5cbfb
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-08-14 16:31:36 +0300

    Update server.sh to include backend.rwady.com setup instructions and enhance deployment process for Laravel 11 project on VPS server.

--- server.sh
+++ server.sh
@@ -8,7 +8,36 @@ git pull
 ssh -t root@82.198.225.45 "cd /home/ahmed-albakor-rwady-backend/htdocs/rwady-backend.ahmed-albakor.com && git pull"
 
 
+backend.rwady.com
+
+cd /
+cd home/rwady-backend/htdocs/backend.rwady.com
+git pull
+
+
 
 5213 7203 0423 8582 
 01/32
 642
+
+
+// setup laravel 11 project on vps server
+
+1 .cloen the project
+2. install composer
+3. create database and user
+4. create .env file
+ - set database connection
+ - set app key // php artisan key:generate
+ - set app url
+ - set app debug
+ - set app timezone
+ - set app locale
+5. set Domin Root Directory to public folder
+6. change permissions of storage/framework and storage/logs
+7. run migrations // php artisan migrate
+8. run seeders // php artisan db:seed
+9. storage link // 
+  // php artisan storage:link
+  // change permissions of storage/app permissions to 777
+  // chown root:root storage/app -R
