9ed3ad44477652cbb35266d1ace579c10aec3dc5
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-09-11 15:23:44 +0300

    Update RefundOrderRequest validation to make 'attached' field nullable, improving flexibility in refund submissions.

--- app/Http/Requests/Order/RefundOrderRequest.php
+++ app/Http/Requests/Order/RefundOrderRequest.php
@@ -12,7 +12,7 @@ class RefundOrderRequest extends BaseFormRequest
             'amount' => 'required|numeric|min:0',
             'reason' => 'required|string|max:255',
             'method' => 'required|string|in:qi,transfer,cash',
-            'attached' => 'string|max:255',
+            'attached' => 'nullable|string|max:255',
         ];
     }
 }
